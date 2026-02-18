2f7a2abef360148aeefd83c48f97166863d47c78
gittestsite1 <gittestsite1@gmail.com>
2026-02-18 12:22:27 +0900

    Repository까지 구현한 구성

--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @ResponseBody
 public class ProductController {
 
+    @Autowired
     private ProductService productService;
 
     ProductController(){
--- src/main/java/com/example/demo/ProductService.java
+++ src/main/java/com/example/demo/ProductService.java
@@ -1,11 +1,16 @@
 package com.example.demo;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
     public String getProduct(){
-        return "NoteBook_Test4";
+        return productRepository.getProduct();
     }
 }
